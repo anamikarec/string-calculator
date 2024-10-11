9911f2bb008f21f2fca2ce714029f94f6960f8c6
Anamika Gupta <anamika@byldd.com>
2024-10-11 20:37:07 +0530

    Add method handles single and two numbers

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,6 +1,11 @@
 class StringCalculator {
   static add(numbers) {
+    // return 0 in case of empty string
     if (numbers === "") return 0;
+
+    // split the input string into an array of numbers
+    const numberArray = numbers.split(",").map((num) => parseInt(num));
+    return numberArray.reduce((sum, num) => sum + num, 0);
   }
 }
 
--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -3,3 +3,13 @@ const StringCalculator = require("./StringCalculator");
 test("should return 0 for empty string", function () {
   expect(StringCalculator.add("")).toBe(0);
 });
+
+test("should return the number itself if one number is provided", () => {
+  expect(StringCalculator.add("1")).toBe(1);
+  expect(StringCalculator.add("2")).toBe(2);
+});
+
+test("should return the sum of two numbers", () => {
+  expect(StringCalculator.add("1,2")).toBe(3);
+  expect(StringCalculator.add("5,7")).toBe(12);
+});
